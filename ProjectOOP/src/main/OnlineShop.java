4b15c6900df2532c8437f26471dd2c344dca0248
Armat <armat.islambekovv@gmail.com>
2025-02-14 00:09:35 +0500

    аа

--- ProjectOOP/src/main/OnlineShop.java
+++ ProjectOOP/src/main/OnlineShop.java
@@ -3,27 +3,27 @@ package main;
 import repository.ProductRepository;
 import services.CartService;
 import services.FAQService;
-import services.AddProductService;
 import models.Product;
 
 import java.sql.SQLException;
 import java.util.Scanner;
+
 public class OnlineShop {
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         ProductRepository productRepository = new ProductRepository();
         CartService cartService = new CartService();
         FAQService faqService = new FAQService();
-        AddProductService addProductService = new AddProductService();  // Создаем сервис для добавления продукта
+
         while (true) {
             System.out.println("\n===== Online Shop Menu =====");
             System.out.println("1. Show Products");
             System.out.println("2. Add Product to Cart");
             System.out.println("3. Show Cart");
             System.out.println("4. Show FAQs");
-            System.out.println("5. Add Product");
-            System.out.println("6. Exit");
+            System.out.println("5. Exit");
             System.out.print("Choose an option: ");
+
             int choice = scanner.nextInt();
 
             switch (choice) {
@@ -49,10 +49,6 @@ public class OnlineShop {
                     faqService.showFAQs();
                     break;
                 case 5:
-                    // Вызов сервиса для добавления нового продукта
-                    addProductService.addProduct();
-                    break;
-                case 6:
                     System.out.println("Exiting...");
                     return;
                 default:
